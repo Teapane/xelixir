a5d8bdacaddeedfedd025e622af40d283b2206e5
Tyler Long <tyler.stephen.long@gmail.com>
2016-02-04 19:05:33 -0700

    Join path in test_helper.exs.
    - Test helper couldn't find the files in the exercises
    subdirectory.
    - Change path in travis.yml

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,12 +1,16 @@
-
 project_root = File.cwd!
-active_problems = "config.json" |> File.read! |> Poison.decode! |> Dict.get "problems"
+
+active_problems = "config.json"
+|> File.read!
+|> Poison.decode!
+|> Dict.get("problems")
 
 Enum.each active_problems, fn dir ->
-    File.cd! dir
-    IO.puts "loading tests for " <> dir
-    "*test.ex*" |> Path.wildcard |> List.first |> Code.require_file
-    File.cd! project_root
+  Path.join(["exercises", dir])
+  |> File.cd!
+  IO.puts "loading tests for " <> dir
+  "*test.ex*" |> Path.wildcard |> List.first |> Code.require_file
+  File.cd! project_root
 end
 
 ExUnit.start()
